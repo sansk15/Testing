f4b1516fcc1c1839ff2fcadbdc4e6219bd745c69
sansk15 <126869204+sansk15@users.noreply.github.com>
2023-03-03 17:07:21 +0530

    Update A.java

--- Git/src/com/demo/A.java
+++ Git/src/com/demo/A.java
@@ -5,8 +5,14 @@ public class A {
 	public static void main(String[] args) 
 	{
 		System.out.println("This is git");
+	
 		// TODO Auto-generated method stub
 
+	}
+	public void m1()
+	{
+	System.out.println("Hello san")
+	
 	}
 
 }
